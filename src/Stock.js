e5fe3de4a8a2101b6c126e899416b26ff86b916a
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-10-19 09:41:33 +0530

    Created stock plot using plotly

---
+++ src/Stock.js
@@ -0,0 +1,73 @@
+import React from "react";
+// import Plot from "react-plotly.js";
+import Plotly from "plotly.js-basic-dist";
+
+import createPlotlyComponent from "react-plotly.js/factory";
+const Plot = createPlotlyComponent(Plotly);
+
+class Stock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockChartXValues: [],
+      stockChartYValues: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchStock();
+  }
+
+  fetchStock() {
+    const pointerToThis = this;
+    console.log(pointerToThis);
+    const API_KEY = "MZ42WBP783C2TXAB";
+    let StockSymbol = "FB";
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
+    let stockChartYValuesFunction = [];
+
+    fetch(API_Call)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
+        for (var key in data["Time Series (Daily)"]) {
+          stockChartXValuesFunction.push(key);
+          stockChartYValuesFunction.push(
+            data["Time Series (Daily)"][key]["1. open"]
+          );
+        }
+        //
+        pointerToThis.setState({
+          stockChartXValues: stockChartXValuesFunction,
+          stockChartYValues: stockChartYValuesFunction,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Stock Market</h1>
+        <Plot
+          data={[
+            {
+              x: this.state.stockChartXValues,
+              y: this.state.stockChartYValues,
+              type: "scatter",
+              mode: "lines+markers",
+              marker: { color: "red" },
+            },
+            // { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+          ]}
+          layout={{ width: 720, height: 440, title: "A Fancy Plot" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Stock;
